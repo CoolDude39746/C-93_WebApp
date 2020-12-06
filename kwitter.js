561c92c45359cb23f336ca4b34c2d6319f92a0c5
CoolDude39746 <71583552+CoolDude39746@users.noreply.github.com>
2020-12-06 12:15:28 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -3,3 +3,22 @@ function login_adduser() {
     localStorage.setItem("User Name",username);
     window.location="kwitter_room.html";
 }
+
+//ADD YOUR FIREBASE LINKS
+var firebaseConfig = {
+    apiKey: "AIzaSyBNjRuxuFFldpQJRWWsy8nHB6TmxYGlIhE",
+    authDomain: "c-94-bd08f.firebaseapp.com",
+    databaseURL: "https://c-94-bd08f-default-rtdb.firebaseio.com",
+    projectId: "c-94-bd08f",
+    storageBucket: "c-94-bd08f.appspot.com",
+    messagingSenderId: "915706782042",
+    appId: "1:915706782042:web:3b107c43dbfb0e486e1363"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+function AddUser() {
+    var Username=document.getElementById("input_1").value;
+    firebase.database().ref("/").child(Username).update({
+    purpose : "to add more users" });
+}
