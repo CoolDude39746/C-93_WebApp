fb149ff9014f5e40c2021430dd7c62e5a44ea1d8
CoolDude39746 <71583552+CoolDude39746@users.noreply.github.com>
2020-12-06 10:52:10 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+
+//ADD YOUR FIREBASE LINKS HERE 
+var firebaseConfig = {
+      apiKey: "AIzaSyDDfBeC8vkRfS_ko4MeLkSujptP8j0DuVY",
+      authDomain: "kwitter-firebase.firebaseapp.com",
+      projectId: "kwitter-firebase",
+      storageBucket: "kwitter-firebase.appspot.com",
+      messagingSenderId: "735127417925",
+      appId: "1:735127417925:web:1dca05cedf531824c3df2c"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
